7be71ae949c4c88bcb51b84386a46438b30a7241
Jesse Gollub <jesse.gollub@ewe.net>
2023-12-05 22:53:12 +0100

    added example project

---
+++ examples/ApiLocksmithExample/Endpoint.cs
@@ -0,0 +1,9 @@
+﻿using FastEndpoints;
+
+namespace ApiLocksmithExample;
+
+public sealed class Endpoint : EndpointWithoutRequest
+{
+    public override void Configure() => Get("protected");
+    public override async Task HandleAsync(CancellationToken ct) => await SendAsync("You are authorized!", cancellation: ct);
+}
