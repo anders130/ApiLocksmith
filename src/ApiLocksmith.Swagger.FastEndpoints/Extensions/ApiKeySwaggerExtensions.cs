630ef33a1f30dbc083abb5867dc5be1815e6b9a3
Jesse Gollub <jesse.gollub@ewe.net>
2023-12-05 22:40:27 +0100

    added swagger project

---
+++ src/ApiLocksmith.Swagger.FastEndpoints/Extensions/ApiKeySwaggerExtensions.cs
@@ -0,0 +1,36 @@
+﻿using ApiLocksmith.Core.Options;
+using FastEndpoints.Swagger;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NSwag;
+
+namespace ApiLocksmith.Swagger.FastEndpoints.Extensions;
+
+/// <summary>
+/// Extension methods for configuring Swagger with ApiKey authentication in FastEndpoints.
+/// </summary>
+public static class ApiKeySwaggerExtensions
+{
+    /// <summary>
+    /// Adds Swagger document configuration with ApiKey authentication.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add the Swagger configuration to.</param>
+    /// <param name="config">The configuration containing ApiKey settings.</param>
+    /// <param name="authConfigSectionKey">The configuration section key for ApiKey authentication.</param>
+    /// <returns>The modified <see cref="IServiceCollection"/>.</returns>
+    public static IServiceCollection AddSwaggerDocumentWithApiKeyAuth(this IServiceCollection services, IConfiguration config, string authConfigSectionKey)
+    {
+        var apiKeyOptions = ApiKeyOptionsLoader.LoadFromConfiguration(config, authConfigSectionKey);
+        services.SwaggerDocument(o =>
+        {
+            o.EnableJWTBearerAuth = false;
+            o.DocumentSettings = s => s.AddAuth(apiKeyOptions.ApiKeySchemeName, new OpenApiSecurityScheme
+            {
+                Name = apiKeyOptions.ApiKeyHeaderName,
+                In = OpenApiSecurityApiKeyLocation.Header,
+                Type = OpenApiSecuritySchemeType.ApiKey,
+            });
+        });
+        return services;
+    }
+}
